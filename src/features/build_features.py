55afd8159f314b138359fddbaaf76038ebeea6d7
Erik Wallinder-Mahler <e.f.w.wallindermaehler@student.tudelft.nl>
2024-05-02 16:39:48 +0200

    Basic DVC pipeline outline

---
+++ src/features/build_features.py
@@ -0,0 +1,10 @@
+def main():
+    with open("data/train","w") as f:
+        pass
+    with open("data/test","w") as f:
+        pass
+    with open("data/val","w") as f:
+        pass
+    
+if __name__ == '__main__':
+    main()
---
+++ src/models/train_model.py
@@ -0,0 +1,6 @@
+def main():
+    with open("models/phishing_model.h5","w") as f:
+        pass
+
+if __name__ == "__main__":
+    main()
