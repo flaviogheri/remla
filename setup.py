8fee8ba8eb01b07cb61bb57164bfd2cacb97f734
Flavio Gheri <f.a.gheri@student.tudelft.nl>
2024-05-02 13:59:37 +0200

    configured cookie_cutter layout

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='development of pipeline management and model_training system',
+    author='team_1',
+    license='MIT',
+)
