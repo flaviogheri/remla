afe5c11b001674f5573923a58234a14294020dd1
Flavio Gheri <f.a.gheri@student.tudelft.nl>
2024-05-04 13:13:14 +0200

    created config.yml and added it to preprocess

--- src/data/preprocess_data.py
+++ src/data/preprocess_data.py
@@ -1,11 +1,20 @@
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
+import yaml
 
+
+## open config.yml
+
+with open("config.yml", "r") as file: 
+    config = yaml.safe_load(file)
+
+training_path = config['data_paths']['training_file']
 
 ### Load data
 
-train = [line.strip() for line in open("/kaggle/input/dl-dataset/DL Dataset/train.txt", "r").readlines()[1:]]
+with open(training_path, "r") as train_file:
+    train = [line.strip() for line in train_file.readlines()[1:]]
 
 
 ### Preprocess data
