e37dade7546f2affdacd16ffa99aa0a9a0f51548
Flavio Gheri <f.a.gheri@student.tudelft.nl>
2024-05-05 18:26:30 +0200

    saving figures and results into reports/figures section

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,20 +1,24 @@
 # models/predict_model.py
 
-
-# UNSURE ABOUT THIS SECTION :: DO I NOT WANT TO JUST HAVE THIS OUTPUT WHATEVER
-# INPUT ONE GIVES INSTEAD OF DOING IT FOR THE TEST DATA ONLY ?
-
+import os
+import yaml
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from tensorflow.keras.models import load_model
-import yaml
 
 
 # Load the configuration (if needed)
 with open("config.yml", "r") as file:
     config = yaml.safe_load(file)
+
+
+# Check if figures directories exist: 
+os.makedirs("reports", exist_ok=True)
+os.makedirs("reports/figures", exist_ok=True)
+
 
 # Load the test data
 x_test = np.load("data/processed/x_test.npy")
@@ -25,26 +29,28 @@ y_test = y_test.reshape(-1, 1)
 # Load the trained model
 model = load_model("models/phishing_model.h5")
 
-
-### Part below : Believed to be unnecessary ...
-
-
-
 # Generate predictions
 y_pred = model.predict(x_test, batch_size=1000)
 
 # Convert predicted probabilities to binary labels
 y_pred_binary = (np.array(y_pred) > 0.5).astype(int)
 
-# Calculate the classification report
 report = classification_report(y_test, y_pred_binary)
-print("Classification Report:")
-print(report)
+
+with open("reports/classification_report.txt", "w") as file:
+    file.write("Classification Report:\n")
+    file.write(report)
 
 # Calculate the confusion matrix
 confusion_mat = confusion_matrix(y_test, y_pred_binary)
-print("Confusion Matrix:\n", confusion_mat)
-print("Accuracy:", accuracy_score(y_test, y_pred_binary))
+with open("reports/confusion_matrix.txt", "w") as file:
+    file.write("Confusion Matrix:\n")
+    file.write(np.array_str(confusion_mat))
+
+# Save accuracy to a file
+accuracy = accuracy_score(y_test, y_pred_binary)
+with open("reports/accuracy.txt", "w") as file:
+    file.write(f"Accuracy: {accuracy}")
 
 # Visualize the confusion matrix using a heatmap
 plt.figure(figsize=(8, 6))
@@ -52,4 +58,5 @@ sns.heatmap(confusion_mat, annot=True, fmt="d", cmap="Blues", xticklabels=["Nega
 plt.xlabel("Predicted")
 plt.ylabel("Actual")
 plt.title("Confusion Matrix Heatmap")
-plt.show()
+plt.savefig("reports/figures/confusion_matrix_heatmap.png")
+plt.close()
